0b7c69dacfc9362f8c81d0601bd4295a57ee3a55
sinsincoccr <145324925+sinsincoccr@users.noreply.github.com>
2024-11-20 00:43:46 +0900

    Add files via upload

---
+++ config.js
@@ -0,0 +1,21 @@
+export const WEDDING_INVITATION_URL = "http://localhost:8000/";
+export const KAKAOTALK_API_TOKEN = "JavaScript 키 입력";
+export const KAKAOTALK_SHARE_IMAGE =
+  "https://cdn.pixabay.com/photo/2014/11/13/17/04/heart-529607_960_720.jpg";
+
+export const WEDDING_DATE = "2025년 04월 05일, 토요일 오전 12시 00분";
+export const WEDDING_LOCATION = "호텔수성, 6층 체플홀";
+
+export const GROOM_NAME = "신형균";
+export const GROOM_ACCOUNT_NUMBER = "국민은행 698-915-131519";
+export const GROOM_FATHER_NAME = "신진수";
+export const GROOM_FATHER_ACCOUNT_NUMBER = "신한은행 732-112-0156153";
+export const GROOM_MOTHER_NAME = "이지선";
+export const GROOM_MOTHER_ACCOUNT_NUMBER = "신한은행 732-112-0156153";
+
+export const BRIDE_NAME = "황윤주";
+export const BRIDE_ACCOUNT_NUMBER = "우리은행 056-135-112098";
+export const BRIDE_FATHER_NAME = "황기수";
+export const BRIDE_FATHER_ACCOUNT_NUMBER = "국민은행 642-151-7765321";
+export const BRIDE_MOTHER_NAME = "김지수";
+export const BRIDE_MOTHER_ACCOUNT_NUMBER = "국민은행 227-864-130594";
